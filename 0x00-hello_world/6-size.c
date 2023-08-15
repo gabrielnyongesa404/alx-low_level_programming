881e5ed1f67933ed84c9c1a24d8f4aec4bc6d0da
gabrielnyongesa404 <gabrielnyongesa404@gmail.com>
2023-08-14 17:29:58 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,7 +5,9 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
+
 {
 	int a;
 	long int b;
@@ -17,4 +19,6 @@ int main(void)
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	
 	return (0);
+}
