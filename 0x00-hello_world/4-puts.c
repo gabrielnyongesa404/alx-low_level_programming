55d51c3ef19497c3c92e399c42207c4582a4c388
gabrielnyongesa404 <gabrielnyongesa404@gmail.com>
2023-08-14 17:59:03 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Entry point
- * 
+ *
  * Return: 0 (Success)
  */
 
@@ -10,6 +10,6 @@ int main(void)
 {
 
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 
 }
